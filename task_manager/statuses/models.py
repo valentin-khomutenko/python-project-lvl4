7f8209bd4ca7dfe530ec66916fa2436365d3d7bd
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2021-05-02 19:16:30 +0300

    reworked filter form template

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -4,6 +4,7 @@ from django.db import models
 MAX_STATUS_NAME_LENGTH = 100
 
 
+# TODO: localisation for fields
 class Status(models.Model):
     name = models.CharField(max_length=MAX_STATUS_NAME_LENGTH, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
