e51aa4147b1ece444a922fae8cb529a81d0667fd
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2021-05-05 19:06:13 +0300

    fixed locales for label CRUD

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('Name'))
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name=_('Created at'))
 
     def __str__(self):
         return self.name
--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -7,7 +7,7 @@ MAX_STATUS_NAME_LENGTH = 100
 # TODO: localisation for fields
 class Status(models.Model):
     name = models.CharField(max_length=MAX_STATUS_NAME_LENGTH, unique=True, verbose_name=_('Name'))
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name=_('Created at'))
 
     def __str__(self):
         return self.name
