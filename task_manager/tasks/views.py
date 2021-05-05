bc9deff30cdead418ce84b8601818bcbdbc0169c
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2021-05-05 19:19:40 +0300

    fixed locales for create task

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, UpdateView
 from django_filters.views import FilterView  # type: ignore
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as _
 
-from task_manager.mixins.views import RaiseUnprocessableEnittyIfInvalidMixin
+from task_manager.mixins.views import RaiseUnprocessableEnittyIfInvalidMixin, \
+    SuccessMessageDeleteMixin
 from task_manager.tasks import models
 from task_manager.tasks import filters
 
@@ -13,26 +16,29 @@ class ListTasks(LoginRequiredMixin, FilterView):
     template_name = 'tasks/list.html'
 
 
-class CreateTask(LoginRequiredMixin, RaiseUnprocessableEnittyIfInvalidMixin, CreateView):
+class CreateTask(LoginRequiredMixin, RaiseUnprocessableEnittyIfInvalidMixin, SuccessMessageMixin, CreateView):
     model = models.Task
     template_name = 'tasks/create.html'
     fields = ('name', 'description', 'executor', 'status', 'labels',)
     success_url = reverse_lazy('list_tasks')
+    success_message = _('Task has been created')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class DeleteTask(LoginRequiredMixin, DeleteView):
+class DeleteTask(LoginRequiredMixin, SuccessMessageDeleteMixin, DeleteView):
     model = models.Task
     template_name = 'tasks/delete.html'
     success_url = reverse_lazy('list_tasks')
+    success_message = _('Task has been deleted')
 
 
-class UpdateTask(LoginRequiredMixin, UpdateView):
+class UpdateTask(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     model = models.Task
     template_name = 'tasks/update.html'
     success_url = reverse_lazy('list_tasks')
+    success_message = _('Task has been updated')
 
     fields = ('name', 'description', 'executor', 'status', 'labels',)
