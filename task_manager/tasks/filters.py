0bf5fb2e0cf6f174651d822b681042d595c681ad
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2021-05-05 19:51:56 +0300

    fixed locales on task filter

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,16 +1,24 @@
 from django import forms
 
+from task_manager.labels.models import Label
 from task_manager.tasks import models
-from django_filters import FilterSet, BooleanFilter  # type: ignore
+from django_filters import FilterSet, BooleanFilter, ModelChoiceFilter  # type: ignore
+from django.utils.translation import gettext as _
 
 
 class TaskFilter(FilterSet):
     self_tasks = BooleanFilter(
-        label='self_tasks', method='filter_self_tasks', widget=forms.CheckboxInput)
+        label=_('Self tasks'), method='filter_self_tasks', widget=forms.CheckboxInput
+    )
+    label = ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        field_name='labels',
+        label=_('Label'),
+    )
 
     class Meta:
         model = models.Task
-        fields = ('status', 'executor', 'labels')
+        fields = ('status', 'executor', 'label', 'self_tasks')
 
     def filter_self_tasks(self, queryset, name, value):
         if value:
