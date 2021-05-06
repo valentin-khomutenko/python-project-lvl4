10581b327db2d4a85e21bc5b3afb66bdfaf2dc50
Valentin Khomutenko <valentine.khomutenko@gmail.com>
2021-05-06 18:00:06 +0300

    fixed statuses being deletable if in use

--- tests/test_statuses.py
+++ tests/test_statuses.py
@@ -3,6 +3,7 @@ import django.test
 from http import HTTPStatus
 
 from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
 
 
 @pytest.mark.django_db
@@ -54,6 +55,26 @@ def test_delete_status_fails_if_not_logged_in(client: django.test.Client):
     response = client.post(f'/statuses/{status.pk}/delete/')
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == f'/login/?next=/statuses/{status.pk}/delete/'
+
+    status.refresh_from_db()
+    assert status.pk is not None
+
+
+@pytest.mark.django_db
+def test_delete_status_fails_if_in_use(logged_in_client: django.test.Client, make_test_user):
+    status = Status(name='Done')
+    status.save()
+
+    user, _ = make_test_user(username='author')
+    task = Task(status=status, name='task', author=user)
+    task.save()
+
+    response = logged_in_client.get(f'/statuses/{status.pk}/delete/')
+    assert response.status_code == HTTPStatus.OK
+
+    response = logged_in_client.delete(f'/statuses/{status.pk}/delete/')
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.url == f'/statuses/'
 
     status.refresh_from_db()
     assert status.pk is not None
